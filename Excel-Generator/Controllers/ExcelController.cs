2b13fd434466c682859c498f6b35df6b8d42db91
Afonso Angeli Christofoletti <afonsoangeli@hotmail.com>
2021-09-01 21:23:26 -0300

    Add project files.

---
+++ Excel-Generator/Controllers/ExcelController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Excel_Generator.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ExcelController : ControllerBase
+    {
+        private readonly ILogger<ExcelController> _logger;
+
+        public ExcelController(ILogger<ExcelController> logger)
+        {
+            _logger = logger;
+        }    
+    }
+}
